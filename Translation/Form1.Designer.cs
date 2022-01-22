e8c99d304b6c0beed7bce33b8ab797e82be60c32
lunarythorn <60178774+lunarythorn@users.noreply.github.com>
2022-01-22 07:52:20 +0300

    commit

---
+++ Translation/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace Translation
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.languageToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.englishToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.russianToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.frenchToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            resources.ApplyResources(this.menuStrip1, "menuStrip1");
+            this.menuStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.languageToolStripMenuItem});
+            this.menuStrip1.Name = "menuStrip1";
+            // 
+            // languageToolStripMenuItem
+            // 
+            resources.ApplyResources(this.languageToolStripMenuItem, "languageToolStripMenuItem");
+            this.languageToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.englishToolStripMenuItem,
+            this.russianToolStripMenuItem,
+            this.frenchToolStripMenuItem});
+            this.languageToolStripMenuItem.Name = "languageToolStripMenuItem";
+            this.languageToolStripMenuItem.CheckStateChanged += new System.EventHandler(this.languageToolStripMenuItem_CheckedChanged);
+            // 
+            // englishToolStripMenuItem
+            // 
+            resources.ApplyResources(this.englishToolStripMenuItem, "englishToolStripMenuItem");
+            this.englishToolStripMenuItem.Checked = true;
+            this.englishToolStripMenuItem.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.englishToolStripMenuItem.Name = "englishToolStripMenuItem";
+            this.englishToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // russianToolStripMenuItem
+            // 
+            resources.ApplyResources(this.russianToolStripMenuItem, "russianToolStripMenuItem");
+            this.russianToolStripMenuItem.Name = "russianToolStripMenuItem";
+            this.russianToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // frenchToolStripMenuItem
+            // 
+            resources.ApplyResources(this.frenchToolStripMenuItem, "frenchToolStripMenuItem");
+            this.frenchToolStripMenuItem.Name = "frenchToolStripMenuItem";
+            this.frenchToolStripMenuItem.Click += new System.EventHandler(this.englishToolStripMenuItem_Click);
+            // 
+            // label1
+            // 
+            resources.ApplyResources(this.label1, "label1");
+            this.label1.Name = "label1";
+            // 
+            // label2
+            // 
+            resources.ApplyResources(this.label2, "label2");
+            this.label2.Name = "label2";
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.BackColor = System.Drawing.Color.Firebrick;
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = false;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // label3
+            // 
+            resources.ApplyResources(this.label3, "label3");
+            this.label3.Name = "label3";
+            // 
+            // label4
+            // 
+            resources.ApplyResources(this.label4, "label4");
+            this.label4.Name = "label4";
+            // 
+            // label5
+            // 
+            resources.ApplyResources(this.label5, "label5");
+            this.label5.Name = "label5";
+            // 
+            // label6
+            // 
+            resources.ApplyResources(this.label6, "label6");
+            this.label6.Name = "label6";
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // label10
+            // 
+            resources.ApplyResources(this.label10, "label10");
+            this.label10.Name = "label10";
+            // 
+            // label9
+            // 
+            resources.ApplyResources(this.label9, "label9");
+            this.label9.Name = "label9";
+            // 
+            // label8
+            // 
+            resources.ApplyResources(this.label8, "label8");
+            this.label8.Name = "label8";
+            // 
+            // label7
+            // 
+            resources.ApplyResources(this.label7, "label7");
+            this.label7.Name = "label7";
+            // 
+            // label11
+            // 
+            resources.ApplyResources(this.label11, "label11");
+            this.label11.Name = "label11";
+            // 
+            // label12
+            // 
+            resources.ApplyResources(this.label12, "label12");
+            this.label12.Name = "label12";
+            // 
+            // label13
+            // 
+            resources.ApplyResources(this.label13, "label13");
+            this.label13.Name = "label13";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.label13);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label11);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem languageToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem englishToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem russianToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem frenchToolStripMenuItem;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Translation/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Translation
+{
+    public partial class Form1 : Form
+    {
+        string currLang = "English";
+        private void SetCulture(string culture)
+        {
+            // Make the CultureInfo.
+            CultureInfo culture_info = new CultureInfo(culture);
+
+            // Make a ComponentResourceManager.
+            ComponentResourceManager component_resource_manager
+                = new ComponentResourceManager(this.GetType());
+
+            // Apply resources to the form.
+            component_resource_manager.ApplyResources(
+                this, "$this", culture_info);
+
+            // Apply resources to the form's controls.
+            foreach (Control ctl in this.Controls)
+            {
+                component_resource_manager.ApplyResources(
+                    ctl, ctl.Name, culture_info);
+            }
+        }
+        public Form1()
+        {
+            
+            CultureInfo culture = new CultureInfo("en-US");
+            Thread.CurrentThread.CurrentCulture = culture;
+            Thread.CurrentThread.CurrentUICulture = culture;
+            InitializeComponent();
+
+            var usTimeZone = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
+
+            TimeZone curTimeZone = TimeZone.CurrentTimeZone;
+            DateTime universalTime = curTimeZone.ToUniversalTime(DateTime.Now);
+
+            var usTime = TimeZoneInfo.ConvertTimeFromUtc(universalTime, usTimeZone);
+            
+            label13.Text = usTime.Hour.ToString() + "." + usTime.Minute.ToString() + "." + usTime.Second.ToString();
+        }
+
+        private void languageToolStripMenuItem_CheckedChanged(object sender, EventArgs e)
+        {
+            MessageBox.Show("test");
+        }
+
+        private void englishToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem menuItem = sender as ToolStripMenuItem;
+            if (menuItem.CheckState == CheckState.Unchecked)
+            {
+                foreach (ToolStripMenuItem item in languageToolStripMenuItem.DropDownItems)
+                {
+                    item.Checked = false;
+                }
+                menuItem.CheckState = CheckState.Checked;
+                switch (menuItem.Name)
+                {
+                    case "englishToolStripMenuItem":
+                        SetCulture("en-US");
+                        languageToolStripMenuItem.Text = "Language";
+                        englishToolStripMenuItem.Text = "English";
+                        russianToolStripMenuItem.Text = "Russian";
+                        frenchToolStripMenuItem.Text = "French";
+                        currLang = "English";
+                        break;
+                    case "russianToolStripMenuItem":
+                        SetCulture("ru-RU");
+                        languageToolStripMenuItem.Text = "Язык";
+                        englishToolStripMenuItem.Text = "Английский";
+                        russianToolStripMenuItem.Text = "Русский";
+                        frenchToolStripMenuItem.Text = "Французский";
+                        currLang = "Russian";
+                        break;
+                    case "frenchToolStripMenuItem":
+                        SetCulture("fr");
+                        languageToolStripMenuItem.Text = "Langue";
+                        englishToolStripMenuItem.Text = "Anglais";
+                        russianToolStripMenuItem.Text = "Russe";
+                        frenchToolStripMenuItem.Text = "Français";
+                        currLang = "French";
+                        break;
+                }
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            TimeZone curTimeZone = TimeZone.CurrentTimeZone;
+            switch (currLang)
+            {
+                case "English":
+                    var usTimeZone = TimeZoneInfo.FindSystemTimeZoneById("US Eastern Standard Time");
+
+                    DateTime universalTime = curTimeZone.ToUniversalTime(DateTime.Now);
+
+                    var usTime = TimeZoneInfo.ConvertTimeFromUtc(universalTime, usTimeZone);
+                    label13.Text = usTime.Hour.ToString() + "." + usTime.Minute.ToString() + "." + usTime.Second.ToString();
+                    break;
+                case "Russian":
+                    var MoscowTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Russian Standard Time");
+
+                    DateTime universalTime2 = curTimeZone.ToUniversalTime(DateTime.Now);
+
+                    var moscowTime = TimeZoneInfo.ConvertTimeFromUtc(universalTime2, MoscowTimeZone);
+                    label13.Text = moscowTime.Hour.ToString() + "." + moscowTime.Minute.ToString() + "." + moscowTime.Second.ToString();
+                    break;
+                case "French":
+                    var EuropeTimeZone = TimeZoneInfo.FindSystemTimeZoneById("Central Europe Standard Time");
+
+                    DateTime universalTime3 = curTimeZone.ToUniversalTime(DateTime.Now);
+
+                    var parisTime = TimeZoneInfo.ConvertTimeFromUtc(universalTime3, EuropeTimeZone);
+                    label13.Text = parisTime.Hour.ToString() + "." + parisTime.Minute.ToString() + "." + parisTime.Second.ToString();
+                    break;
+            }
+
+        }
+
+    }
+}
